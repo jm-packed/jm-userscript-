ff32503a49c38d8baeb1df3e12fe72092e78e416
jm-packed <gianskysalino@gmail.com>
2025-03-16 10:05:54 -0400

    Update jm userscript.user.js

--- jm userscript.user.js
+++ jm userscript.user.js
@@ -1,177 +1,29 @@
 // ==UserScript==
-2
-
-3
 // @name         Jm bypass
-4
-
-5
 // @namespace    jm-bypass-userscript
-6
-
-7
 // @version      1.6
-8
-
-9
 // @description  Userscript made by Jm
-10
-
-11
 // @author       jm
-12
-
-13
 // @match        *://linkvertise.com/*
-14
-
-// @match        *://auth.platorelay.com/*
-
-15
 // @match        *://work.ink/*
-16
-
-17
-// @match        *://linkvertise.com/*/*
-18
-
-// @match        *://rekonise.com/*
-
-// @match        *://rekonise.com/*/*
-
-19
-// @match        *://workink.net/*
-20
-
-21
-// @match        *://r.work.ink/*
-22
-
-23
 // @match        *://*.*/s?*
-24
-
-// @match        *://keyrblx.com/*/*
-
-25
 // @match        *://keyrblx.com/*
-26
-
-27
+// @match        *://rekonise.com/*
 // @match        *://mobile.codex.lol/*
-28
-
-29
 // @match        *://pandadevelopment.net/getkey*
-30
-
-31
 // @match        *://ads.luarmor.net/get_key*
-32
-
-33
-// @match        https://plato-gateway-static.pages.dev/b
-34
-
-35
-// @match        https://gateway.platoboost.com/cdn-cgi/trace?url=*
-36
-
-37
-// @match        https://auth.platoboost.net/*
-38
-
-39
-// @match        https://gateway.platoboost.com/a/*?id=*
-40
-
-41
-// @match        https://gateway.platoboost.com/*
-42
-
-43
-// @match        https://gateway.platoboost.com/a/2569?id=*
-44
-
-45
-// @match        https://loot-link.com/s?*
-46
-
-47
-// @match        https://loot-links.com/s?*
-48
-
-49
-// @match        https://lootlink.org/s?*
-50
-
-51
-// @match        https://lootlinks.co/s?*
-52
-
-53
-// @match        https://lootdest.info/s?*
-54
-
-55
-// @match        https://lootdest.org/s?*
-56
-
-57
-// @match        https://lootdest.com/s?*
-58
-
-59
-// @match        https://links-loot.com/s?*
-60
-
-61
-// @match        https://linksloot.net/s?*
-62
-
-63
 // @match        https://spdmteam.com/key-system-2?hwid=*
-64
-
-65
 // @match        https://spdmteam.com/key-system-3?hwid=*
-66
-
-67
+// @match        https://krnl.cat/checkpointv2/vgetkey?hash=*
+// @match      https://auth.platorelay.com/*
+// @match      https://auth.platoboost.net/*
 // @require      https://raw.githubusercontent.com/jm-packed/jm-userscript-/refs/heads/main/jm%20userscript%20main.js
-68
-
-69
 // @grant        GM_xmlhttpRequest
-70
-
-71
 // @grant        GM_setClipboard
-72
-
-73
 // @grant        GM_openInTab
-74
-
-75
 // @connect      helya.pylex.xyz
-76
-
-77
 // @homepageURL  https://discord.gg/S2U9rEaKc3
-78
-
-79
 // @downloadURL  https://raw.githubusercontent.com/jm-packed/jm-userscript-/main/jm%20userscript.user.js
-80
-
-81
 // @updateURL    https://raw.githubusercontent.com/jm-packed/jm-userscript-/main/jm%20userscript.user.js
-82
-
-83
 // @icon         https://i.imgur.com/onJQllI.png
-84
-
-85
 // ==/UserScript==
