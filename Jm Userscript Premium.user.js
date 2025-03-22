1394a15225ea3c5cd239fbffed4a715fbcae3bd0
jm-packed <gianskysalino@gmail.com>
2025-03-22 05:19:19 -0400

    Update Jm Userscript Premium.user.js

--- Jm Userscript Premium.user.js
+++ Jm Userscript Premium.user.js
@@ -50,9 +50,9 @@
 
 // @homepageURL  https://discord.gg/S2U9rEaKc3
 
-// @downloadURL  https://raw.githubusercontent.com/jm-packed/jm-userscript-/refs/heads/main/jm%20userscript%20main.user.js
+// @downloadURL  https://raw.githubusercontent.com/jm-packed/jm-userscript-/refs/heads/main/Jm%20Userscript%20Premium.user.js
 
-// @updateURL    https://raw.githubusercontent.com/jm-packed/jm-userscript-/refs/heads/main/jm%20userscript%20main.user.js
+// @updateURL    https://raw.githubusercontent.com/jm-packed/jm-userscript-/refs/heads/main/Jm%20Userscript%20Premium.user.js
 
 // ==/UserScript==
 
